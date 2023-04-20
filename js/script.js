15af1bed49fff1537ff9b25ccac6213d2c078936
marcos <masadu@outlook.es>
2023-04-20 12:09:38 +0200

    boton de colores

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
 
+
+const body = document.querySelector("body")
+const btn= document.querySelector(".btn")
+
+btn.addEventListener("click", function(e) {
+const randocomor = colors[Math.floor(colors.length * Math.random())]
+
+console.log("me has hecho click")
+
+body.style.backgroundColor=randocomor
+
+})
